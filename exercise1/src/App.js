acb045622fd76f5b7b125fcae05733baa167d04f
SammyB <samanthabrown146@msn.com>
2020-09-24 21:33:47 +0100

    just commiting

---
+++ exercise1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 style={{color: "black"}}>REACT Building an app</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
