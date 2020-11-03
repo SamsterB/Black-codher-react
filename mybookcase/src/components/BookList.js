4a8778306f14e83b7fc986192e7410e8c286ac3e
SammyB <samanthabrown146@msn.com>
2020-11-03 06:44:24 +0000

    added the remove button information.

--- mybookcase/src/components/BookList.js
+++ mybookcase/src/components/BookList.js
@@ -7,7 +7,7 @@ const BookList = (props) => {
    
     return (
     <div>
-    {props.books.map(book => <Book key={book.id} book={book} addBook= {props.addBook} />)}
+    {props.books.map(book => <Book key={book.id} books={book} addBook= {props.addBook} removeBook= {props.removeBook} />)}
     </div>
     );
    }
