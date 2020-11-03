18d8afd8c7ca787890e5bfffec1037223514ae04
SammyB <samanthabrown146@msn.com>
2020-11-03 06:45:08 +0000

    Trying to move the Header around so that it is centered on the page.

--- mybookcase/src/components/Header.js
+++ mybookcase/src/components/Header.js
@@ -4,10 +4,13 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return (
         <React.Fragment>
-            <h1>My Bookcase</h1>
+        <div className='container mt-10'>
+            <h1 className="text-primary mb-3">My Bookcase</h1>
+        </div>
             <Link to="/"> Home </Link>
             <Link to="/bookcase">Bookcase</Link>
             <Link to="/About">About</Link>
+        
         </React.Fragment>
     )
 }
